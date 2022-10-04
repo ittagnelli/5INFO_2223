139336b9751d0fd8068007d0d8569df6ba6dfebf
Chindris Alexandru <alexandruioan.chindris@istitutoagnelli.it>
2022-10-04 14:45:53 +0200

    Consegna esercitazione 01

---
+++ RETI/Esercitazioni_Compiti/chindris_alexandru/Esercizio_01/chindris_es1_byte_order.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <arpa/inet.h>
+
+int main(){
+    int numbers[] ={1000, 12890, 54163, 34012, 62984};
+    int numbers1[] ={19875, 5499, 59821, 43120, 26489};
+    for(int x = 0; x < 5; x++){
+        printf("\n Da host a Network order %x", ntohs(numbers[x]));
+        printf("\n Da network a host order %x", htonl(numbers1[x]));
+    }
+}

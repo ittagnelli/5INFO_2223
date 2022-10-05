1cb6d661fdd05f80856acbe274139a770d1ef45b
Chindris Alexandru <alexandruioan.chindris@istitutoagnelli.it>
2022-10-05 12:37:03 +0200

    correxione bug

--- RETI/Esercitazioni_Compiti/chindris_alexandru/Esercizio_01/chindris_es1_byte_order.c
+++ RETI/Esercitazioni_Compiti/chindris_alexandru/Esercizio_01/chindris_es1_byte_order.c
@@ -6,6 +6,8 @@ int main(){
     int numbers1[] ={19875, 5499, 59821, 43120, 26489};
     for(int x = 0; x < 5; x++){
         printf("\n Da host a Network order %x", ntohs(numbers[x]));
+    }
+    for(int y = 0; y < 5; y++){
         printf("\n Da network a host order %x", htonl(numbers1[x]));
     }
 }

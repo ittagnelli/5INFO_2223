c44e1964f00276e932b9afd649e906b74016ede6
Matteo-Sola <76943413+Matteo-Sola@users.noreply.github.com>
2022-10-25 10:47:04 +0200

    Esercizio consegnato

---
+++ TPSI/Esercitazioni_Compiti/sola_matteo/Sola_esercizio_js_9d.js
@@ -0,0 +1,11 @@
+function sommaNumeri(parametro){
+    let somma = 0;
+    for(let i = 1; i <= +parametro; i++){
+        somma = somma + i;
+    }
+    return somma;
+}
+
+console.log(`La somma del primo numero è ${sommaNumeri(5)}`);
+console.log(`La somma del primo numero è ${sommaNumeri(10)}`);
+console.log(`La somma del primo numero è ${sommaNumeri(15)}`);

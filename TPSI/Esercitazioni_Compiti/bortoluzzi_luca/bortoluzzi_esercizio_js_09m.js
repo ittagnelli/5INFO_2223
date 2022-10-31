1a400696f2bbdb3dffccc37c8590f01e9d0a2060
Luca Bortoluzzi <luca.bortoluzzi@istitutoagnelli.it>
2022-10-31 20:42:03 +0100

    es_TPSI

---
+++ TPSI/Esercitazioni_Compiti/bortoluzzi_luca/bortoluzzi_esercizio_js_09m.js
@@ -0,0 +1,25 @@
+Somma = (a,b) => {
+    return a+b;
+ } 
+ sottrazione = (a,b) => {
+     return a-b;
+ } 
+ moltiplicazione = (a,b) => {
+     return a*b;
+ } 
+ divisione = (a,b) => {
+     return a/b;
+ } 
+ 
+ console.log("risultato Somma:");
+ console.log(Somma(34,4));
+ console.log(Somma(4334,435));
+ console.log("risultato sottrazione:");
+ console.log(sottrazione(4674,6));
+ console.log(sottrazione(4343,547));
+ console.log("risultato moltiplicazione:");
+ console.log(moltiplicazione(4,1));
+ console.log(moltiplicazione(56,34));
+ console.log("risultato divisione:");
+ console.log(divisione(30,12));
+ console.log(divisione(35,7));

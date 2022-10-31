645b4490eb4e49eff526da29aa65c48f03bd813d
William-Viteritti <Wicuzz19@gmail.com>
2022-10-31 11:23:03 -0400

    Upload

---
+++ TPSI/Esercitazioni_Compiti/william_viteritti/Viteritti_esercizio_js_09h.js
@@ -0,0 +1,7 @@
+function max(a, b){
+    a > b ? console.log(a,"è il valore maggiore"):console.log(b,"è il valore maggiore");
+}
+
+console.log(max(3,4));
+console.log(max(8,5));
+console.log(max(12,24));
---
+++ TPSI/Esercitazioni_Compiti/william_viteritti/Viteritti_esercizio_js_09m.js
@@ -0,0 +1,25 @@
+somma = (a,b) => {
+    return a+b;
+ } 
+ sottrazione = (a,b) => {
+     return a-b;
+ } 
+ moltiplicazione = (a,b) => {
+     return a*b;
+ } 
+ divisione = (a,b) => {
+     return a/b;
+ } 
+ 
+ console.log("Il risultato della somma è:");
+ console.log(somma(3,2));
+ console.log(somma(4,7));
+ console.log("Il risultato della sottrazione è:");
+ console.log(sottrazione(4,1));
+ console.log(sottrazione(120,5));
+ console.log("Il risultato della moltiplicazione è:");
+ console.log(moltiplicazione(5,5));
+ console.log(moltiplicazione(7,8));
+ console.log("Il risultato della divisione è:");
+ console.log(divisione(20,5));
+ console.log(divisione(16,8));

62c579ec71afcd17b1f1a13f0c05ea3759bbe0e4
William-Viteritti <Wicuzz19@gmail.com>
2022-11-02 14:59:44 -0400

    Upload

--- TPSI/Esercitazioni_Compiti/william_viteritti/Viteritti_esercizio_js_09e.js
+++ TPSI/Esercitazioni_Compiti/william_viteritti/Viteritti_esercizio_js_09e.js
@@ -1,23 +1,44 @@
-let Risultato = function(h,m,s,ms){
-    this.h = h;
-    this.m = m;
-    this.s = s;
-    this.ms = ms;
+let day1 = {
+    h:12,
+    m:30,
+    s:05
 }
 
-let day1 = new Date("2022", "11", "10", "24", "35", "49", "16");
-let day2 = new Date("2022", "01", "25", "14", "20", "12", "10");
-let day3 = new Date("2022", "06", "04", "23", "45", "29", "05");
-let day4 = new Date("2022", "08", "07", "15", "10", "20", "02");
-
-console.log(diff_g(day1, day2));
-console.log(diff_g(day3, day4));
-
-function diff_g (day1, day2){
-    let h = day1.getHours() - day2.getHours();
-    let m = day1.getMinutes() - day2.getMinutes();
-    let s = day1.getSeconds() - day2.getSeconds();
-    let ms = day1.getMilliseconds() - day2.getMilliseconds();
-    let risultato = new Risultato(h,m,s,ms);
-    return risultato;
+let day2 = {
+    h:23,
+    m:40,
+    s:10
 }
+
+let day3 = {
+    h:12,
+    m:35,
+    s:23
+}
+
+let day4 = {
+    h:16,
+    m:23,
+    s:00
+}
+
+let risultato;
+
+function differenza_giorni(day1,day2){
+    let g1 = new Date(2022, 12, 12, day1.h, day1.m, day1.s, 0);
+    let g2 = new Date(2022, 12, 12, day2.h, day2.m, day2.s, 0);
+
+
+    return risultato = {
+        diff_h: ((g2-g1)/(60*60*1000)),
+        diff_m:((g2-g1)/(60*1000)),
+        diff_s:((g2-g1)/(1000)),
+        diff_ms:(g2-g1)
+    }
+}
+
+risultato = differenza_giorni(day1, day2);
+console.log(`La differenza tra day1 e day2 in ms è di ${risultato.diff_ms}, in secondi è di ${risultato.diff_s}, la differenza in minuti è di: ${risultato.diff_m} ed in ore è di ${risultato.diff_h}`);
+
+risultato = differenza_giorni(day3, day4);
+console.log(`La differenza tra day3 e day4 in ms è di ${risultato.diff_ms}, in secondi è di ${risultato.diff_s}, la differenza in minuti è di: ${risultato.diff_m} ed in ore è di ${risultato.diff_h}`);
--- TPSI/Esercitazioni_Compiti/william_viteritti/Viteritti_esercizio_js_09f.js
+++ TPSI/Esercitazioni_Compiti/william_viteritti/Viteritti_esercizio_js_09f.js
@@ -1,16 +1,11 @@
-let n = "4";
-
-console.log(Fibonacci((+n)+1));
-console.log(Fibonacci((+n)+2));
-console.log(Fibonacci((+n)+3));
-console.log(Fibonacci((+n)+4));
-console.log(Fibonacci((+n)+5));
-
 function Fibonacci(n){
-    if(n==0 || n == 1){
+    if(+n == 0 || +n == 1){
         return 1;
     }
-
-    return (Fibonacci(n-1)+Fibonacci(n-2));
-    
+    return Fibonacci(+n-1)+Fibonacci(+n-2);
 }
+console.log("Successione di Fibonacci ennesima:",Fibonacci(3));
+console.log("Successione di Fibonacci ennesima:",Fibonacci(5));
+console.log("Successione di Fibonacci ennesima:",Fibonacci(8));
+console.log("Successione di Fibonacci ennesima:",Fibonacci(10));
+console.log("Successione di Fibonacci ennesima:",Fibonacci(12));

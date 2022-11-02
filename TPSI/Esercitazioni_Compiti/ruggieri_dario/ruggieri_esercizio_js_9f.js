57dc620e3bcbefa085354557bb50f785c9a74ffe
Ruggieri Dario <dario.ruggieri@istitutoagnelli.it>
2022-11-02 16:38:57 +0100

    aggiunta fattoriale all'esercizio 9f

--- TPSI/Esercitazioni_Compiti/ruggieri_dario/ruggieri_esercizio_js_9f.js
+++ TPSI/Esercitazioni_Compiti/ruggieri_dario/ruggieri_esercizio_js_9f.js
@@ -10,3 +10,8 @@ console.log("Fibonacci ennesima 6: ",Fibonacci(6));
 console.log("Fibonacci ennesima 7: ",Fibonacci(7)); 
 console.log("Fibonacci ennesima 8: ",Fibonacci(8)); 
 console.log("Fibonacci ennesima 9: ",Fibonacci(9)); 
+
+/*
+function fattoriale(n){
+    if(n == 0 || n == 1) return 1
+    return n * fattoriale(n-1)*/

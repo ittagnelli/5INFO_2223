97967d026fd33ab5b76e6cb10f41e75e191e9e8a
Simone Laudani <simone.laudani@istitutoagnelli.it>
2022-11-08 11:56:47 +0100

    es10

---
+++ TPSI/Esercitazioni_Compiti/laudani_simone/laudani_esercizio_js_10a.js
@@ -0,0 +1,11 @@
+let str = "Massimiliano Allegri";
+
+
+function abbrevCognome(str) {
+    let str2 = str.split(' ')
+    let str3 = str2[1].slice(0, 1);
+
+    console.log(str2[0] + " " + str3 + ".")
+}
+
+abbrevCognome(str);

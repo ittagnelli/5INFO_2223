d8c920191265687d53fbdb7e8f129034cb8d44a7
Michele-Siracusa <michelesiracusa2@gmail.com>
2022-11-14 21:21:28 +0100

    Esercizio js_10n

---
+++ TPSI/Esercitazioni_Compiti/siracusa_michele/siracusa_esercizio_js_10n.js
@@ -0,0 +1,8 @@
+function insert(first, second, N){
+    return first.slice(0,N) + " " + second + first.slice(N);
+
+ }
+ 
+ console.log(insert("Facciamo tanti esercizi che ci fanno bene", "JavaScript", 23));
+
+

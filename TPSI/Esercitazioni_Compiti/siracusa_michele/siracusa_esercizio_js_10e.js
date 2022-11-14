fb9c3fb27dcc55c3922af4d47c7d6c4237143886
Michele-Siracusa <michelesiracusa2@gmail.com>
2022-11-14 19:40:03 +0100

    Esercizio js_10e

---
+++ TPSI/Esercitazioni_Compiti/siracusa_michele/siracusa_esercizio_js_10e.js
@@ -0,0 +1,14 @@
+function capitalize_all(str){
+    let words = str.split(' '); 
+    for(let i = 0; i < words.length; i++){
+        words[i] = (words[i].charAt(0).toUpperCase()).concat(words[i].slice(1))
+        
+    }
+    
+    return words.join(' '); 
+    
+}
+
+console.log(capitalize_all("ciao mondo la terra gira!!!"))
+
+

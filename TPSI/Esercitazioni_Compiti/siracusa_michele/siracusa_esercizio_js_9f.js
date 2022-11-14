74a57563ad62aa143f38f901b9d949f04f1e42b8
Michele-Siracusa <michelesiracusa2@gmail.com>
2022-11-14 19:30:07 +0100

    Delete siracusa_esercizio_js_9f.js

--- TPSI/Esercitazioni_Compiti/siracusa_michele/siracusa_esercizio_js_9f.js
+++
@@ -1,9 +0,0 @@
-function Fibonacci(N){
-    return (+N == 0 || +N == 1) ?  1 : (Fibonacci(+N-1) + Fibonacci(+N-2))
-    
-}
-
-
-console.log(Fibonacci(3));
-console.log(Fibonacci(5));
-console.log(Fibonacci(6))

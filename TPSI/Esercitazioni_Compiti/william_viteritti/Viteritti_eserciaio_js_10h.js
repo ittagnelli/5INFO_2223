963df2cda0bad6909c7c5b72eab42b6c536379bf
William-Viteritti <76515071+William-Viteritti@users.noreply.github.com>
2022-11-15 09:25:09 +0100

    Delete Viteritti_eserciaio_js_10h.js

--- TPSI/Esercitazioni_Compiti/william_viteritti/Viteritti_eserciaio_js_10h.js
+++
@@ -1,8 +0,0 @@
-function protect_email(str,l){
-
-    str = str.slice(0, l+1);
-    str = str.concat("...");
-    return str;
-}
-
-console.log(protect_email("william.viteritti@istitutoagnelli.it", 10));

4d9bacf16ceeee883d67934ca61eb4a3b3c643be
Ruggieri Dario <dario.ruggieri@istitutoagnelli.it>
2022-12-13 11:33:02 +0100

    es 11 second version

--- TPSI/Esercitazioni_Compiti/ruggieri_dario/ruggieri_esercizio_js_11.js
+++ TPSI/Esercitazioni_Compiti/ruggieri_dario/ruggieri_esercizio_js_11.js
@@ -1,8 +1,19 @@
 let numeri = [1,2,3,4];
 let stringhe = ["ciao", "come", "stai"];
-function reverse(numeri){
-    
+function reverse(numeri){ 
     return numeri.sort((el1, el2)=> numeri.indexOf(el2)- numeri.indexOf(el1))
 }
 
 console.log(reverse(stringhe));
+
+
+function reverse2(numeri){
+    let Reverse = [];
+    numeri.forEach(item => {
+        Reverse.unshift(item);
+    })
+    return Reverse;
+}
+
+
+// console.log(reverse2(stringhe));

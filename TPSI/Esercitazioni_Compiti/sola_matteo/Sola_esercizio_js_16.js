bf0eea94abb19531d2acc2645753a7340e708e39
Matteo-Sola <76943413+Matteo-Sola@users.noreply.github.com>
2022-12-15 16:45:53 +0100

    Esercizio consegnato

---
+++ TPSI/Esercitazioni_Compiti/sola_matteo/Sola_esercizio_js_16.js
@@ -0,0 +1,17 @@
+var library = [ 
+    { author: 'Bill Gates', title: 'The Road Ahead', libraryID: 1254},
+    { author: 'Steve Jobs', title: 'Walter Isaacson', libraryID: 4264},
+    { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', libraryID: 3245}
+];
+
+function ordina_decrescente(library) {
+    let ordinamento = library.sort((t1, t2) => {
+        if(t2.title > t1.title) return 1;
+        else if (t2.title < t1.title) return -1;
+        else return 0;
+    });
+    
+    console.log("Autori ordinati per titolo: ", ordinamento);
+}
+
+ordina_decrescente(library);

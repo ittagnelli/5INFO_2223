68136c6afc90b2a29d43759779a855d0de128b57
William-Viteritti <76515071+William-Viteritti@users.noreply.github.com>
2023-03-05 21:49:18 +0100

    Delete Viteritti_esercizio_js_32.js

--- TPSI/Esercitazioni_Compiti/william_viteritti/Viteritti_esercizio_js_32.js
+++
@@ -1,9 +0,0 @@
-function Riempi_vettore(N,M){
-    
-    return Array.from({length: N}, () => Math.floor(Math.random() * M)+1);
-        
-   }
-   
-   let N = 3;
-   let M = 50;
-   console.log(Riempi_vettore(N,M));

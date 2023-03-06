c2dde42220284730341bea32009c6e6249978b0d
Marco-Garabello <115414398+Garabello-Marco@users.noreply.github.com>
2023-03-06 16:43:32 +0100

    Delete garabello_esercizio_js_32.js

--- TPSI/Esercitazioni_Compiti/garabello_marco/garabello_esercizio_js_32.js
+++
@@ -1,8 +0,0 @@
-function rem_anag(array){
-    let map = new Map();
-    array.forEach((el,idx) => map.set(idx,new Set(el)));
-    console.log(map)
-}
-
-
-console.log(rem_anag(['omar', 'sette', 'roma', 'teste', 'mora', 'colazione', 'locazione']));

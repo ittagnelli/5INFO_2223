a40306293e83b2f8868e113687fde6bac7e0740d
Michele-Siracusa <michelesiracusa2@gmail.com>
2023-03-23 18:46:56 +0100

    Esercizio js_39

---
+++ TPSI/Esercitazioni_Compiti/siracusa_michele/siracusa_esercizio_js_39.js
@@ -0,0 +1,14 @@
+let numbers = [3, 5, 4, 2, 6, 1];
+
+
+let one = numbers[0];
+let two = numbers[1];
+let three = numbers[2];
+let four = numbers[3];
+let five = numbers[4];
+let six = numbers[5];
+
+
+console.log(`One: ${one}, Two: ${two}, Three: ${three}, Four: ${four}, Five: ${five}, Six: ${six}`);
+
+

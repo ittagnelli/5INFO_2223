c5dd239ee2b8777ba5a8f61ce375dea1fb62b21d
Matteo-Sola <76943413+Matteo-Sola@users.noreply.github.com>
2023-03-24 16:36:39 +0100

    Esercizio consegnato

---
+++ TPSI/Esercitazioni_Compiti/sola_matteo/Sola_esercizio_js_38.js
@@ -0,0 +1,13 @@
+let item = ["Egg", 0.25, 12];
+
+// let name = item[0];
+// let price = item[1];
+// let quantity = item[2];
+
+// console.log(`Item: ${name}, Quantity: ${quantity}, Price: ${price}`);
+
+let [name, price, quantity] = item;
+
+console.log(name);
+console.log(price);
+console.log(quantity);

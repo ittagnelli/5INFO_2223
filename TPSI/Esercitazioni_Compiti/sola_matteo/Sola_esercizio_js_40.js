bdf508300aa99f9629978c4edfb54a6aab76c760
Matteo-Sola <76943413+Matteo-Sola@users.noreply.github.com>
2023-03-27 20:23:37 +0200

    Esercizio consegnato

---
+++ TPSI/Esercitazioni_Compiti/sola_matteo/Sola_esercizio_js_40.js
@@ -0,0 +1,6 @@
+const students = ['Christina', 'Jon', 'Alexandre'];
+
+//let [firstName, secondName, lastName] = students;
+let [ , , lastName] = students;
+
+console.log(`L'ultimo studente e' ${lastName}`);

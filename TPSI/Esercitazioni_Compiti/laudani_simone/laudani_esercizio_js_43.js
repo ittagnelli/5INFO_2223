6d1fa4f4bb7ec23dd1ad17cd4d85bbcfc72cb8cc
Simone-Laudani <90993768+Simone-Laudani@users.noreply.github.com>
2023-04-18 10:41:20 +0200

    es43

--- TPSI/Esercitazioni_Compiti/laudani_simone/laudani_esercizio_js_43.js
+++ TPSI/Esercitazioni_Compiti/laudani_simone/laudani_esercizio_js_43.js
@@ -15,8 +15,28 @@ class Poligono {
       let area = (perimetro * apotema) / 2;
       return area;
     }
-  }
-  
+}
+
+class Quadrato extends Poligono {
+    constructor(nlati, lunghezza) {
+      super();
+      this.nlati = nlati;
+      this.lunghezza = lunghezza;
+    }
+    perimetro() {
+      let perimetro = this.nlati ** 2;
+      return perimetro;
+    }
+    area() {
+      let area = (this.nlati * 2);
+      return area;
+    }
+}
+
 let poligono = new Poligono(4, 10);
 console.log(poligono.perimetro());
 console.log(poligono.area());
+
+let quadrato = new Quadrato(4, 5);
+console.log("perimetro quadrato: " + quadrato.perimetro());
+console.log("area quadrato: " + quadrato.area());

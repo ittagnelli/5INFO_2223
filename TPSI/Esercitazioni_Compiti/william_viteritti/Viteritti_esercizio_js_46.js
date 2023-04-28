59775092db801718298b17eac7866247bc3f392b
William-Viteritti <76515071+William-Viteritti@users.noreply.github.com>
2023-04-28 20:45:42 +0200

    Add files via upload

---
+++ TPSI/Esercitazioni_Compiti/william_viteritti/Viteritti_esercizio_js_46.js
@@ -0,0 +1,47 @@
+class Automobile{
+    constructor(modello, targa, posti, colore, cavalli){
+        this.modello = modello;
+        this.targa = targa;
+        this.posti = posti;
+        this.colore = colore;
+        this.cavalli = cavalli;
+    }
+
+    id(){
+        return console.log(`Il modello dell'auto è ${this.modello} e la targa è ${this.targa}`);
+    }
+
+    design(){
+        return console.log(`L'automobile ha ${this.posti} posti ed il colore è ${this.colore}`);
+    }
+
+    motore(){
+        return console.log(`L'automobile ha ${this.cavalli} cavalli`);
+    }
+
+}
+
+let auto = new Automobile("Audi", "QWERTY","5","blue","300");
+auto.id();
+auto.design();
+auto.motore();
+
+let auto1 = new Automobile("Rolls Royce", "UIOPAS","5","white","700");
+auto1.id();
+auto1.design();
+auto1.motore();
+
+let auto2 = new Automobile("Smart", "GHJKLN","2","red","100");
+auto2.id();
+auto2.design();
+auto2.motore();
+
+let auto3 = new Automobile("Skoda", "FGHJKF","5","blue","400");
+auto3.id();
+auto3.design();
+auto3.motore();
+
+let auto4 = new Automobile("Panda", "ERTYIJ","4","grey","200");
+auto4.id();
+auto4.design();
+auto4.motore();

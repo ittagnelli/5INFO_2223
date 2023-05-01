9ca081fffd6ea4a3ee9b898912acf4c49973071b
Federico-Pomba <76780839+Federico-Pomba@users.noreply.github.com>
2023-05-01 23:10:02 +0200

    Delete es42.js

--- TPSI/Esercitazioni_Compiti/es42.js
+++
@@ -1,8 +0,0 @@
-class calcolatrice {
-
-    somma(dist) { console.log(); return this; }
-    sottraio(dist) { console.log(`vado a sud di ${dist} metri`); return this; }
-   add(value){
-    this.a + this.b;
-   }
-}
